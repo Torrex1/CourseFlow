52df72d27f07c1c1c233c20b0e2a3d924e492994
Torrex1 <flendik666@gmail.com>
2025-11-03 23:22:48 +0300

    create component for svg icons

---
+++ app/components/ui/BaseIcon.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+	name: { type: String, required: true },
+	size: { type: String, default: 20 },
+	class: { type: String, default: "" },
+});
+
+const iconUrl = computed(() => defineAsyncComponent (() => import(`@/assets/icons/${props.name}.svg`)));
+</script>
+
+<template>
+	<component
+		:is="iconUrl"
+		:class="class"
+		:style="{ width: props.size + 'px', height: props.size + 'px' }"
+	/>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint"],
+	modules: [
+		"@nuxtjs/tailwindcss",
+		"@nuxt/eslint",
+		"nuxt-svgo-loader",
+	],
 	devtools: { enabled: true },
+	css: ["~/assets/css/tailwind.css"],
 	compatibilityDate: "2025-07-15",
 	eslint: {
 		config: {
