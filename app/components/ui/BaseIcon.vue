b511c871ea66f825c0ae7816b22f58e61d7f3e53
Torrex1 <flendik666@gmail.com>
2025-11-10 00:39:20 +0300

    add svg support and TS to BaseIcon component

--- app/components/ui/BaseIcon.vue
+++ app/components/ui/BaseIcon.vue
@@ -1,19 +1,34 @@
-<script setup>
+<script setup lang="ts">
 import { computed } from "vue";
 
-const props = defineProps({
-	name: { type: String, required: true },
-	size: { type: String, default: 20 },
-	class: { type: String, default: "" },
-});
+const props = withDefaults(defineProps<{
+		name: string;
+		title?: string;
+		size?: string | number;
+		class?: string;
+	}>(), 
+	{
+		size: "20",
+		class: ""
+	}
+)
 
-const iconUrl = computed(() => defineAsyncComponent (() => import(`@/assets/icons/${props.name}.svg`)));
+// Record позволяет объявить, что будет храниться в ключ-значение (icons - string - ключ | path - default - any)
+const icons: Record<string, { default: any }> = import.meta.glob('@/assets/icons/*.svg', { eager: true });
+
+const iconUrl = computed( () => {
+	const key = Object.keys(icons).find(k => k.endsWith(`${props.name}.svg`));
+	return key ? icons[key]?.default : null;
+});
 </script>
 
 <template>
-	<component
-		:is="iconUrl"
-		:class="class"
-		:style="{ width: props.size + 'px', height: props.size + 'px' }"
-	/>
+	<div class="flex gap-1 items-center">
+		<component
+			:is="iconUrl"
+			:class="class"
+			:style="{ width: props.size + 'px', height: props.size + 'px' }"
+		/>
+		<span v-if="props.title">{{ props.title }}</span>
+	</div>
 </template>
