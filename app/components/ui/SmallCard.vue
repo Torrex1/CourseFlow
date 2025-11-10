ba10d25a7cd711485492c6e384e13fa4d089dddd
Torrex1 <flendik666@gmail.com>
2025-11-11 01:45:33 +0300

    create a new small cards-widget ui component

---
+++ app/components/ui/SmallCard.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import BaseIcon from "./BaseIcon.vue";
+
+const props = defineProps<{
+    title: string;
+    score: string;
+    icon: string;
+    bgColor: string;
+}>();
+</script>
+
+<template>
+    <div class="flex gap-4 border w-64 p-3 justify-start items-center rounded-lg">
+        <div :class="['flex justify-center rounded-md w-10 h-10', props.bgColor]">
+            <BaseIcon
+                :name="props.icon"
+            />
+        </div>
+
+        <div class="flex flex-col">
+            <span class="text-lg">{{ props.title }}</span>
+            <span class="font-bold text-xl leading-6">{{ props.score }}</span>
+        </div>
+    </div>
+</template>
